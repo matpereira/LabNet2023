1608f9f8c661e6b4508427fa35560e6fa20ac9d9
matpereira <matiaspereira@Outlook.com>
2023-08-15 16:55:13 -0300

    Mostrar por consola Hello World

---
+++ HelloWorld.Lab2023/HelloWorld.Lab2023/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab2023HelloWorld
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" _   _      _ _         _    _            _     _");
+            Console.WriteLine("| | | |    | | |       | |  | |          | |   | |");
+            Console.WriteLine("| |_| | ___| | | ___   | |  | | ___  _ __| | __| |");
+            Console.WriteLine("|  _  |/ _ \\ | |/ _ \\  | |/\\| |/ _ \\| '__| |/ _` |");
+            Console.WriteLine("| | | |  __/ | | (_) | \\  /\\  / (_) | |  | | (_| |");
+            Console.WriteLine("\\_| |_/\\___|_|_|\\___/   \\/  \\/ \\___/|_|  |_|\\__,_|");
+
+
+        }
+    }
+}
+
