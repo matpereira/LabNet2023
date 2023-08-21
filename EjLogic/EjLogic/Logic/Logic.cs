fa888d1b6cd113057f8c73e5a91b0a7ff487d4c0
matpereira <matiaspereira@Outlook.com>
2023-08-21 18:34:42 -0300

    Logic Agregado

---
+++ EjLogic/EjLogic/Logic/Logic.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjLogic
+{
+    internal class Logic
+    {
+
+        public static void ThrowException()
+        {
+            throw new Exception("Mensaje de excepcion de prueba!");
+        }
+    }
+}
---
+++ EjLogic/EjLogic/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EjLogic;
+
+namespace EjLogic
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            LogicException();
+        }
+
+        private static void LogicException()
+        {
+            try
+            {
+               Logic.ThrowException();
+            }
+            catch (Exception ex)
+            {
+            Console.WriteLine($"Mensaje de la excepcion: {ex.Message}");
+            Console.WriteLine($"Tipo de excepcion: {ex.GetType().ToString()}");
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
