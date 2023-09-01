fbbfb00f028e3d5066497b938b81d4834e63d14e
matpereira <matiaspereira@Outlook.com>
2023-09-01 15:15:51 -0300

    .

---
+++ LinQ/LinQ.Logic/BaseLogic.cs
@@ -0,0 +1,18 @@
+﻿using LinQ.Data;
+using System.Data.Entity;
+using System.Linq;
+
+namespace LinQ.Logic
+{
+    public class BaseLogic
+    {
+        protected readonly NorthwindContext context;
+        public BaseLogic()
+        {
+            context = new NorthwindContext();
+        }
+
+        
+
+    }
+}
--- LinQ/LinQ.UI/Program.cs
+++ LinQ/LinQ.UI/Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+//using LinQ.Entities;
 
 namespace LinQ.UI
 {
@@ -10,6 +8,57 @@ namespace LinQ.UI
     {
         static void Main(string[] args)
         {
+            int seleccion;
+            /*  do
+              {
+                  // Leer la opción del usuario
+                  if (int.TryParse(Console.ReadLine(), out seleccion))
+                  {
+                      switch (seleccion)
+                      {
+                          case 1:
+                              Console.Clear();
+                              break;
+                          case 2:
+                              Console.Clear();
+                              break;
+                          case 3:
+                              Console.Clear();
+                              break;
+                          case 4:
+                              Console.Clear();
+                              break;
+                          case 5:
+                              Console.Clear();
+                              break;
+                          case 0:
+                              Console.ForegroundColor = ConsoleColor.Magenta;
+                              Console.WriteLine("Saliendo del programa...");
+                              break;
+                          default:
+                              Console.WriteLine("Opción no válida. Intente nuevamente.");
+                              break;
+                      }
+                  }
+                  else
+                  {
+                      Console.WriteLine("Por favor, seleccione un digito.");
+                  }
+                  Console.WriteLine();
+              } while (seleccion != 0);
+            */
+            /*
+            var query = from Region in 
+                        where Region.RegionID == 1 || Region.RegionID == 2
+                        select Region;
+
+            seleccion = query.Count();
+            Console.WriteLine($"Cantidad de regiones: {seleccion}");
+            Console.WriteLine();
+
+            */
+
         }
+
     }
 }
