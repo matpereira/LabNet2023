757eb68fb7007098fa89f31a79a8c9f88401d974
matpereira <matiaspereira@Outlook.com>
2023-09-01 14:05:47 -0300

    Configuracion inicial

---
+++ LinQ/LinQ.UI/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinQ.UI
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
