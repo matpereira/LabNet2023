a0f4f5cf7bbdfbd59515db0626c0b97a9d0129dd
matpereira <matiaspereira@Outlook.com>
2023-09-09 12:45:33 -0300

    sql

---
+++ Lab.EF/Lab.EF.MVC/Controllers/ShippersController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using Lab.EF.Logic;
+using Lab.EF.Logic.DTO;
+
+namespace Lab.EF.MVC.Controllers
+{
+    public class ShippersController : Controller
+    {
+        // GET: Shippers
+        public ActionResult Index()
+        {
+            ShippersLogic shipperLogic = new ShippersLogic();
+            List<ShippersDTO> shippers = shipperLogic.GetAll();
+            return View(shippers);
+        }
+    }
+}
